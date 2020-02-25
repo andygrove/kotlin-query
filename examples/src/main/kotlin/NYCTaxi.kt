f2cd5b52512ec3bc4947051d8f75329b461e88ba
Andy Grove <andygrove73@gmail.com>
2020-02-25 11:18:03 -0700

    Save

--- examples/src/main/kotlin/NYCTaxi.kt
+++ examples/src/main/kotlin/NYCTaxi.kt
@@ -5,6 +5,7 @@ import io.andygrove.kquery.logical.cast
 import io.andygrove.kquery.logical.col
 import org.apache.arrow.vector.types.FloatingPointPrecision
 import org.apache.arrow.vector.types.pojo.ArrowType
+import kotlin.system.measureTimeMillis
 
 
 fun main() {
@@ -34,16 +35,20 @@ fun main() {
     congestion_surcharge: Utf8
     */
 
-    val df = ctx.csv("/home/andy/data/yellow_tripdata_2019-01.csv", 1024)
-           .aggregate(
-               listOf(col("passenger_count")),
-               listOf(Max(cast(col("fare_amount"), ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)))))
+    val time = measureTimeMillis {
+        val df = ctx.csv("/home/andy/data/yellow_tripdata_2019-01.csv", 1*1024)
+                .aggregate(
+                        listOf(col("passenger_count")),
+                        listOf(Max(cast(col("fare_amount"), ArrowType.FloatingPoint(FloatingPointPrecision.DOUBLE)))))
 
-    val results = ctx.execute(df)
+        val results = ctx.execute(df)
 
-    results.forEach {
-        println(it.schema)
-        println(it.toCSV())
+        results.forEach {
+            println(it.schema)
+            println(it.toCSV())
+        }
     }
+
+    println("Query took $time ms")
 
 }
